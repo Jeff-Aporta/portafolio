6de1fe3d70e60af67b8a5b06c7d83941a06fd6db
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-08-06 20:48:46 -0500

    codigo con representación add

--- componentes/paginas/main/componentes.jsx
+++ componentes/paginas/main/componentes.jsx
@@ -169,265 +169,96 @@ function Code({
     }
 }
 
-function CodigoConRepresentación({ children, url }) {
-    return <div
-        className={CSScmds(`
-            x<1100px{
-                grid-template-columns: [
-                    repeat(1, 1fr),
-                    repeat(auto-fill, minmax(50%, 1fr))
-                ];
-            }
-        `)}
-        style={{
-            display: 'inline-grid',
-            justifyContent: 'center',
-            alignItems: 'center',
-            maxWidth: '100%',
-        }}
-    >
-        <Code
+function CodigoConRepresentacion({ children, url, p5js = true, titulo }) {
+    return (
+        <Paper
+            elevation={0}
             style={{
-                margin: 0,
-                minWidth: '100%',
+                borderRadius: '30px',
+                padding: '10px',
+                border: '1px solid #444',
+                margin: '20px 0',
             }}
-            height="60vh"
         >
-            {children}
-        </Code>
-        <iframe
-            src={url}
-            style={{
-                height: "60vh",
-                margin: 0,
-                width: "100%",
-            }}
-        />
-    </div>
-}
+            <Typography
+                variant="h4"
+                style={{
+                    margin: "10px",
+                    fontWeight: 'lighter',
+                }}
+            >
+                {titulo}
+            </Typography>
+            <div
+                className={CSScmds(`
+                    x<1100px{
+                        grid-template-columns: [
+                            repeat(1, 1fr),
+                            repeat(auto-fill, minmax(50%, 1fr))
+                        ];
+                    }
+                `)}
+                style={{
+                    position: 'relative',
+                    display: 'inline-grid',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    maxWidth: '100%',
+                    width: '100%',
 
-function removerTabulacionesDeCodigo(str) {
-    let multiLinea = false;
-    if (typeof str == "string") {
-        str = str.split("\n");
-        while (str[0].trim() == "") {
-            str.shift();
-        }
-        while (str[str.length - 1].trim() == "") {
-            str.pop();
-        }
-        if (str.length > 1) {
-            multiLinea = true;
-        }
-
-        let espaciosAlInicio = str.find((linea) => linea.trim()).match(/^\s*/)[0].length ?? 0;
-
-        str = str.map((linea) => {
-            return linea.split("").reduce((acumulado, caracter, index) => {
-                if (index < espaciosAlInicio && caracter == " ") {
-                    return acumulado;
-                }
-                return acumulado + caracter;
-            }, "");
-        }).join("\n");
-    }
-    return {
-        multiLinea,
-        str
-    };
-}
-
-function ChipCode(props) {
-    let label = props.label;
-    delete props.label;
-    return (
-        <Chip
-            {...props}
-            style={{
-                backgroundColor: props.nocode ? "rgba(0,0,0,0.4)" : "black",
-                color: "white",
-            }}
-            label={
+                    borderRadius: '30px',
+                    overflow: 'hidden',
+                }}
+            >
                 <Code
-                    className={`
-                        solo-texto
-                        ${props.nocode ? "nocode" : ""}
-                    `}
-                    linenumbers={false}
-                    esCopiable={false}
-                >
-                    {label}
-                </Code>
-            }
-        />
-    );
-}
-
-function BotonLinkPortafolio() {
-    return (
-        <ThemeProvider theme={themeSelected}>
-            <Paper
-                style={{
-                    textAlign: "center",
-                    padding: "40px",
-
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "stretch",
-                    gap: "20px",
-
-                }}
-            >
-                <Portafolio />
-            </Paper>
-        </ThemeProvider>
-    );
-
-    function Social() {
-        return (
-            <div
-                style={{
-                    display: "inline-flex",
-                    flexDirection: "row",
-                    justifyContent: "space-evenly",
-                    alignItems: "center",
-
-                    gap: "30px",
-
-                    fontSize: "40px",
-
-                    scale: "0.85",
-                }}
-            >
-                <IconoSocial
-                    icono="fa-brands fa-whatsapp"
-                    tooltip="WhatsApp"
-                    outlineColor="green"
-                    url="https://wa.link/1tmqmt"
-                />
-
-                <IconoSocial
-                    icono="fa-brands fa-youtube"
-                    tooltip="YouTube"
-                    outline="8px"
-                    iconoColor="red"
-                    outlineColor="white"
-                    url="https://www.youtube.com/@JeffAporta"
-                />
-            </div>
-        );
-
-        function IconoSocial({ icono, tooltip, iconoColor = "white", outline = "15px", outlineColor = "black", url = "" }) {
-            const pstyle = {
-                position: "absolute",
-                top: "0",
-                left: "0",
-            };
-            return (
-                <TooltipTheme
-                    title={tooltip}
-                    placement="right"
-                >
-                    <Link
-                        href={url}
-                        target="_blank"
-                        underline="none"
-                    >
-                        <span
-                            style={{
-                                position: "relative",
-                            }}
-                        >
-                            <i
-                                className={icono}
-                                style={{
-                                    ...pstyle,
-                                    WebkitTextStroke: `${outline} ${outlineColor}`,
-                                }}
-                            />
-                            <i
-                                className={icono}
-                                style={{
-                                    ...pstyle,
-                                    color: iconoColor,
-                                }}
-                            />
-                            <i
-                                className={icono}
-                                style={{
-                                    opacity: "0",
-                                }}
-                            />
-                        </span>
-                    </Link>
-                </TooltipTheme>
-            );
-        }
-    }
-
-    function Portafolio() {
-        return (
-            <div
-                style={{
-                    display: "inline-block",
-                    padding: "20px",
-                    border: "10px solid #333",
-                    borderRadius: "25px",
-                }}
-            >
-                <div
                     style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        gap: "20px",
+                        margin: 0,
+                        minWidth: '100%',
                     }}
+                    height="60vh"
                 >
-                    <img
-                        src="https://jeff-aporta.github.io/portafolio/src/imgs/jeff-profile.png"
+                    {children}
+                </Code>
+                <div>
+                    <iframe
+                        className={CSScmds(`
+                            x<1100px{
+                                border-radius: (0 0 20px 20px, 0 20px 20px 0);
+                            }
+                        `)}
+                        allow="accelerometer; magnetometer; gyroscope;"
+                        frameBorder="0"
+                        src={url}
                         style={{
-                            background: "black",
-                            width: "100px",
-                            aspectRatio: "1/1",
-                            borderRadius: "50%",
-                            objectFit: "cover",
-                            objectPosition: "right",
-                            backgroundImage: "url(https://jeff-aporta.github.io/portafolio/src/imgs/back.gif)",
-                            backgroundSize: "cover",
-                            backgroundBlendMode: "hard-light",
-                            border: "2px solid white",
+                            height: "60vh",
+                            margin: 0,
+                            width: "100%",
+                            overflow: 'hidden',
                         }}
                     />
-                    <Social />
+                    {(() => {
+                        if (p5js) {
+                            return <img
+                                className={CSScmds(`
+                                    x<1100px{
+                                        top: (,25px);
+                                        bottom: (15px,)
+                                    }
+                                `)}
+                                src="https://jeff-aporta.github.io/portafolio/src/imgs/p5js.svg"
+                                style={{
+                                    borderRadius: '7px',
+                                    position: 'absolute',
+                                    right: "10px",
+                                    background: 'white',
+                                    width: '50px',
+                                    height: '50px',
+                                }}
+                            />
+                        }
+                    })()}
                 </div>
-                <EspacioVertical height="10px" />
-                <Typography variant="h5">
-                    Puedes ver más de mi trabajo en mi
-                </Typography>
-                <EspacioVertical height="10px" />
-                <Button
-                    variant="outlined"
-                >
-                    <Link
-                        href="https://jeff-aporta.github.io/portafolio/"
-                        target="_blank"
-                        underline="none"
-                    >
-                        <Typography variant="h4">
-                            <b>
-                                Portafolio
-                            </b>
-                        </Typography>
-                        <Typography variant="h6">
-                            <small>
-                                (Jeffrey Agudelo)
-                            </small>
-                        </Typography>
-                    </Link>
-                </Button>
             </div>
-        );
-    }
+        </Paper>
+    )
 }
